068c54fe77cee82b061403e4d676bee269392b31
pradyumann <pradyumann@vt.edu>
2021-02-25 15:40:39 -0500

    Done Road,Car and Intersection - RE

--- Parser.py
+++ Parser.py
@@ -1,43 +1,57 @@
 from objects import Road, Intersection,Car
 import queue
-filepath = 'Files/a.txt'
-CarList = []
-RoadList = dict()
-IntersectionList = dict()
 
-with open(filepath) as fp:
-   line = fp.readline()
-   words = line.split()
-   time = int(words[0])
-   numIntersection = int(words[1])
-   numStreet = int(words[2])
-   numCars = int(words[3])
-   score = int(words[4])
-   cnt = 1
-   line = fp.readline()
-   cnt = 2
-   for x in range(numStreet):
-       words = line.split()
-       temp = Road(words[2],int(words[3]))
-       if words[0] not in IntersectionList.keys():
-           IntersectionList[words[0]] = Intersection(words[0])
-       IntersectionList[words[0]].addOutRoad(temp)
+class InitialParser:
+    def __init__(self):
+        filepath = 'Files/a.txt'
+        CarList = []
+        RoadList = dict()
+        IntersectionList = dict()
 
-       if words[1] not in IntersectionList.keys():
-           IntersectionList[words[1]] = Intersection(words[1])
-       IntersectionList[words[1]].addInRoad(temp)
+    def inputFileParse(self):
+        with open(self.filepath) as fp:
+           line = fp.readline()
+           words = line.split()
+           time = int(words[0])
+           numIntersection = int(words[1])
+           numStreet = int(words[2])
+           numCars = int(words[3])
+           score = int(words[4])
+           cnt = 1
+           line = fp.readline()
+           cnt = 2
+           for x in range(numStreet):
+               words = line.split()
+               temp = Road(words[2],int(words[3]))
+               if words[0] not in self.IntersectionList.keys():
+                   self.IntersectionList[words[0]] = Intersection(words[0])
+               self.IntersectionList[words[0]].addOutRoad(temp)
 
-       temp.addFromAndToIntersection(IntersectionList[words[0]], IntersectionList[words[1]])
-       RoadList[words[2]] = temp
-       #
-       line = fp.readline()
-       cnt = cnt + 1
+               if words[1] not in self.IntersectionList.keys():
+                   self.IntersectionList[words[1]] = Intersection(words[1])
+               self.IntersectionList[words[1]].addInRoad(temp)
 
-   for x in range(numCars):
-       words = line.split()
-       num = int(words[0])
-       temp2 = queue.Queue()
-       for j in range(num):
-           temp2.put(RoadList[words[j+1]])
-       Car(temp2)
-       line = fp.readline()
+               temp.addFromAndToIntersection(self.IntersectionList[words[0]], self.IntersectionList[words[1]])
+               self.RoadList[words[2]] = temp
+               #
+               line = fp.readline()
+               cnt = cnt + 1
+
+           for x in range(numCars):
+               words = line.split()
+               num = int(words[0])
+               temp2 = []
+               for j in range(num):
+                   temp2.append(self.RoadList[words[j+1]])
+               Car(temp2)
+               line = fp.readline()
+
+def writer(  ):
+    tfmap = dict()
+    filepath = "output.txt"
+    list = tfmap.toList()
+    with open(filepath) as fp:
+        fp.writelines(len(list))
+        for i in range(len(list)):
+            fp.writelines(list[i])
+            fp.writelines(list[i].out)
