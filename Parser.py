5257b4add52feb4a55c9177d4f9e603919c7576d
pradyumann <pradyumann@vt.edu>
2021-02-25 15:12:14 -0500

    Done Road,Car and Intersection

--- Parser.py
+++ Parser.py
@@ -1,8 +1,9 @@
+from objects import Road, Intersection,Car
+import queue
 filepath = 'Files/a.txt'
-
 CarList = []
-RoadList = []
-IntersectionList = []
+RoadList = dict()
+IntersectionList = dict()
 
 with open(filepath) as fp:
    line = fp.readline()
@@ -17,12 +18,26 @@ with open(filepath) as fp:
    cnt = 2
    for x in range(numStreet):
        words = line.split()
-       print(words)
+       temp = Road(words[2],int(words[3]))
+       if words[0] not in IntersectionList.keys():
+           IntersectionList[words[0]] = Intersection(words[0])
+       IntersectionList[words[0]].addOutRoad(temp)
+
+       if words[1] not in IntersectionList.keys():
+           IntersectionList[words[1]] = Intersection(words[1])
+       IntersectionList[words[1]].addInRoad(temp)
+
+       temp.addFromAndToIntersection(IntersectionList[words[0]], IntersectionList[words[1]])
+       RoadList[words[2]] = temp
+       #
        line = fp.readline()
        cnt = cnt + 1
 
    for x in range(numCars):
        words = line.split()
        num = int(words[0])
-       Car(num, words[1:num+1])
+       temp2 = queue.Queue()
+       for j in range(num):
+           temp2.put(RoadList[words[j+1]])
+       Car(temp2)
        line = fp.readline()
