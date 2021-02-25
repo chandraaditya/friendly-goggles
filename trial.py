8e1cbba5ccaf1ff28bf4be676b8554d96c7331cc
pradyumann <pradyumann@vt.edu>
2021-02-25 13:03:09 -0500

    Reading lines and words

--- trial.py
+++ trial.py
@@ -1,9 +1,15 @@
 print("Just trying 2")
 
-filepath = 'a_example'
+filepath = 'Files/a.txt'
 
 with open(filepath) as fp:
    line = fp.readline()
+   words = line.split()
+   time = int(words[0])
+   numIntersection = int(words[1])
+   numStreet = int(words[2])
+   numCars = int(words[3])
+   score = int(words[4])
    cnt = 1
    while line:
        print(line)
