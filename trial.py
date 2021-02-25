48a356fc09425ef20ae5cecd43b9d22f0d72a1a8
pradyumann <pradyumann@vt.edu>
2021-02-25 11:20:18 -0500

    Reading lines

--- trial.py
+++ trial.py
@@ -1,1 +1,11 @@
 print("Just trying 2")
+
+filepath = 'a_example'
+
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print(line)
+       line = fp.readline()
+       cnt += 1
